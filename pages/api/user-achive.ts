b6f0715760cd61cc6e3a4c2c3fb998823f2d5cbd
Iskariot <blablaname4@gmail.com>
2025-08-12 00:21:17 +0300

    Refactor user achievement handler and update tUserAchive interface to improve data structure and processing logic

--- pages/api/user-achive.ts
+++ pages/api/user-achive.ts
@@ -1,24 +1,46 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import userMock from '@/shared/mocks/user-achivements-data.json';
 import achieveMock from '@/shared/mocks/achivements-data.json';
-import { tAchive, tUser, tUserAchive } from '@/shared/types/achievments';
+import { tAchive, tUserAchive } from '@/shared/types/achievments';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<tUserAchive[]>) {
+const joinAchieves = (): tUserAchive => {
+    const userAchiveIds = new Set(userMock.map((u) => u.achiveId));
+    const userAchieves = achieveMock.filter((a) => userAchiveIds.has(a.id));
+
+    let rangs = { rang_1_Achieves: 0, rang_2_Achieves: 0, rang_3_Achieves: 0 };
+    let proAchieves = [];
+    let platformAchieves = [];
+    const pickAchieveData = (a: tAchive) => ({
+        variant: a.variant,
+        total: a.total,
+        describe: a.describe,
+        name: a.name,
+        tooltip: a.tooltip,
+    });
+
+    for (let a of userAchieves) {
+        if (a.rang === 1) rangs.rang_1_Achieves++;
+        else if (a.rang === 2) rangs.rang_2_Achieves++;
+        else if (a.rang === 3) rangs.rang_3_Achieves++;
+        if (a.type === 'pro') proAchieves.push(pickAchieveData(a));
+        else if (a.type === 'platform') platformAchieves.push(pickAchieveData(a));
+    }
+
+    return {
+        totalCount: achieveMock.length,
+        userAchieveCount: userAchieves.length,
+        proAchieves,
+        platformAchieves,
+        closedAchievesCount: achieveMock.length - userAchieves.length,
+        ...rangs,
+    };
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<tUserAchive>) {
     // TODO
     // const {userId} = req.query
     // userMock.filter(u => u.id === userId)
-    const ids = userMock.map((u: tUser) => u.achiveId);
-    const achieves = achieveMock.filter((a: tAchive) => ids.includes(a.id));
-    const joined = userMock
-        .map((userAch: tUser) => {
-            const fullAch = achieves.find((a: tAchive) => a.id === userAch.achiveId);
-            return {
-                ...fullAch,
-                progress: userAch.progress,
-            };
-        })
-        .filter(Boolean);
-    await new Promise((resolve) => setTimeout(resolve, 1000));
 
-    return res.status(200).json(joined as tUserAchive[]);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return res.status(200).json(joinAchieves() as tUserAchive);
 }
--- shared/types/achievments.ts
+++ shared/types/achievments.ts
@@ -15,6 +15,27 @@ export interface tAchive {
     tooltip: string;
 }
 
-export interface tUserAchive extends tAchive {
-    progress?: number;
+export interface tUserAchive {
+    totalCount: number; // кількість всіх досягнень tAchive
+    userAchieveCount: number; // кількість досягнень юзера
+    closedAchievesCount: number;
+    rang_1_Achieves: number; // кількість досягнень з рангом 1
+    rang_2_Achieves: number; // кількість досягнень з рангом 2
+    rang_3_Achieves: number; // кількість досягнень з рангом 3
+    proAchieves: {
+        // досягнення у яких type==='pro'
+        variant: string;
+        total?: number;
+        describe?: string;
+        name: string;
+        tooltip: string;
+    }[];
+    platformAchieves: {
+        // досягнення у яких type==='platform'
+        variant: string;
+        total?: number;
+        describe?: string;
+        name: string;
+        tooltip: string;
+    }[];
 }
