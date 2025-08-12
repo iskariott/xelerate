943897fb1f505112e141b64dd0dd6b8169fea502
Iskariot <blablaname4@gmail.com>
2025-08-12 21:49:40 +0300

    Refactor AchieveCard component to improve type safety and update styling for better layout

--- modules/statistic/entities/ui/achieve-card/achieve-card.tsx
+++ modules/statistic/entities/ui/achieve-card/achieve-card.tsx
@@ -2,29 +2,30 @@ import Image from 'next/image';
 import st from './achieve-card.module.scss';
 import { ReactNode } from 'react';
 import Tooltip from '@/shared/ui/tooltip/tooltip';
+import type { tAchieveRang } from '@/modules/statistic/shared/types';
 
-type tCardVariant = 'gold' | 'gray' | 'dark';
 type AchieveCardProps = {
     children: ReactNode;
-    variant: tCardVariant;
+    rang: tAchieveRang | 'closed';
     tooltip: string;
 };
 
-const Colors: { [key in tCardVariant]: [string, string] } = {
-    gold: ['rgba(180, 149, 122, 0.6)', 'rgba(105, 83, 63, 0.6)'],
-    dark: ['rgba(40, 40, 40, 0.6)', 'rgba(12, 11, 11, 0.6)'],
-    gray: ['rgba(94, 94, 94, 0.6)', 'rgba(58, 58, 58, 0.6)'],
+const Colors: { [key in tAchieveRang | 'closed']: [string, string] } = {
+    3: ['rgba(180, 149, 122, 0.6)', 'rgba(105, 83, 63, 0.6)'],
+    2: ['rgba(94, 94, 94, 0.6)', 'rgba(58, 58, 58, 0.6)'],
+    1: ['rgba(40, 40, 40, 0.6)', 'rgba(12, 11, 11, 0.6)'],
+    closed: ['rgba(40, 40, 40, 0.6)', 'rgba(12, 11, 11, 0.6)'],
 };
 
-function getGradient(variant: tCardVariant) {
-    return `linear-gradient(180deg, ${Colors[variant][0]} 0%, ${Colors[variant][1]} 100%)`;
+function getGradient(rang: tAchieveRang | 'closed'): string {
+    return `linear-gradient(180deg, ${Colors[rang][0]} 0%, ${Colors[rang][1]} 100%)`;
 }
 
-export default function AchieveCard({ children, variant, tooltip }: AchieveCardProps) {
-    const gradient = { '--gradient': getGradient(variant) };
+export default function AchieveCard({ children, rang, tooltip }: AchieveCardProps) {
+    const gradient = { '--gradient': getGradient(rang) };
     return (
         <div className={st.card} style={gradient as any}>
-            <div className={st.icon}>
+            <div className={[st.icon, rang === 'closed' ? st.closed : ''].join(' ')}>
                 <button>
                     <Tooltip text={tooltip}>
                         <Image src="/images/info.png" width={24} height={24} alt="tips" />
